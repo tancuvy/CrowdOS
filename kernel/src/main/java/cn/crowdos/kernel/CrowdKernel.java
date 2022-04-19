6c8deae369c92880b289f1c4532a48afe34b1a46
loyx <isluoyixuan1@163.com>
2022-04-19 21:17:34 +0800

    modified CrowdKernel

--- kernel/src/main/java/cn/crowdos/kernel/CrowdKernel.java
+++ kernel/src/main/java/cn/crowdos/kernel/CrowdKernel.java
@@ -1,7 +1,27 @@
 package cn.crowdos.kernel;
 
 public class CrowdKernel {
+
+    private boolean initialed = false;
+
+    private static CrowdKernel kernel;
     public static String version(){
         return "CrowdOS cn.crowdos.kernel.Kernel v1.0";
+    }
+
+    private CrowdKernel(){}
+
+    public static CrowdKernel getKernel(){
+        if (kernel != null) return kernel;
+        kernel = new CrowdKernel();
+        return kernel;
+    }
+
+    public void initial(Object...args){
+        initialed = true;
+    }
+
+    public void otherOpera(){
+        if (! initialed) throw new UnsupportedOperationException();
     }
 }
