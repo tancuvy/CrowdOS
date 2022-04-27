b5c6c8e84cc245b5079e93ca5f6e92e4b140ec66
loyx <isluoyixuan1@163.com>
2022-04-27 18:20:30 +0800

    :memo: docs(constraint): add doc of Decomposable

--- kernel/src/main/java/cn/crowdos/kernel/constraint/Decomposable.java
+++ kernel/src/main/java/cn/crowdos/kernel/constraint/Decomposable.java
@@ -1,6 +1,22 @@
 package cn.crowdos.kernel.constraint;
 
-
+/**
+ * <p>The {@code Decomposable} interface must be implement by any class
+ * that needs to be decomposed into smaller parts. Usually decomposable
+ * user-defied constraints (that implement interface {@link Constraint})
+ * need to implement this interface.</p>
+ *
+ * @param <T> the type of element composed by the decomposer
+ * @author loyx
+ * @since 1.0.0
+ * @see Decomposer
+ * @see Constraint
+ */
 public interface Decomposable<T>{
+    /**
+     * Return a decomposer over elements of type {@code T}.
+     *
+     * @return a Decomposer
+     */
     Decomposer<T> decomposer();
 }
