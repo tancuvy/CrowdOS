c1f114e04b14cbda2261a9c9c7aa3b73ef7d422e
loyx <isluoyixuan1@163.com>
2022-06-06 19:22:13 +0800

    :recycle: refactor(resource): refactor ability interface

---
+++ kernel/src/test/java/cn/crowdos/kernel/resource/SimpleParticipantTest.java
@@ -0,0 +1,25 @@
+package cn.crowdos.kernel.resource;
+
+import cn.crowdos.kernel.constraint.Coordinate;
+import cn.crowdos.kernel.constraint.wrapper.IntegerCondition;
+import org.junit.jupiter.api.Test;
+
+import javax.sound.midi.Soundbank;
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SimpleParticipantTest {
+
+    @Test
+    void debug(){
+    }
+
+    @Test
+    void hasAbility() {
+    }
+
+    @Test
+    void getAbility() {
+    }
+}
