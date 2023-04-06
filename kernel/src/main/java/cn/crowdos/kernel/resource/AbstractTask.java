91801a9102d5b70b99695fcf9d1743d1c002f87a
transcend <1803861022@qq.com>
2023-04-06 14:54:33 +0800

    TrustBasedIncentive

--- kernel/src/main/java/cn/crowdos/kernel/resource/AbstractTask.java
+++ kernel/src/main/java/cn/crowdos/kernel/resource/AbstractTask.java
@@ -4,21 +4,30 @@ import cn.crowdos.kernel.Decomposer;
 import cn.crowdos.kernel.constraint.Condition;
 import cn.crowdos.kernel.constraint.Constraint;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public abstract class AbstractTask implements Task{
 
+    protected final long taskId;
+    private static long taskCounter = 1;
     protected final List<Constraint> constraints;
     protected final TaskDistributionType taskDistributionType;
     protected TaskStatus status;
 
     protected AbstractTask(List<Constraint> constraints, TaskDistributionType taskDistributionType) {
+        this.taskId = taskCounter++;
         this.constraints = constraints;
         this.taskDistributionType = taskDistributionType;
     }
 
 
     abstract public Decomposer<Task> decomposer();
+
+    public long getTaskId() {
+        return this.taskId;
+    }
 
     @Override
     public TaskDistributionType getTaskDistributionType() {
