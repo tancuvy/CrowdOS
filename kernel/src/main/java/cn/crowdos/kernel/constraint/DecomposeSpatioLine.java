1eff8168540c9e2f7d4e3510508eb0e32655e4fe
lelezhao <lele_zhao1208@163.com>
2023-04-08 21:08:58 +0800

    :sparkles: feat(Add ConstraintTests):

--- kernel/src/main/java/cn/crowdos/kernel/constraint/DecomposeSpatioLine.java
+++ kernel/src/main/java/cn/crowdos/kernel/constraint/DecomposeSpatioLine.java
@@ -11,30 +11,20 @@ public class DecomposeSpatioLine {
     public DecomposeSpatioLine(double width,List<Coordinate> pointsList) throws InvalidConstraintException {
         this.pointsList = pointsList;
         this.width = width;
-        DecomposePointslistToLines(pointsList);
     }
 
     public DecomposeSpatioLine(double width,Coordinate...pointsList) throws InvalidConstraintException {
         this(width,Arrays.asList(pointsList));
-        DecomposePointslistToLines(Arrays.asList(pointsList));
     }
 
-    private void DecomposePointslistToLines(List<Coordinate> pointsList) throws InvalidConstraintException {
+    public List<SpatioLine> DecomposePointslistToLines() throws InvalidConstraintException {
+        List<SpatioLine> decomposeLines = new ArrayList<>(pointsList.size()-1);
         for(int i=0;i<pointsList.size()-1;i++){
             Coordinate startPoint = pointsList.get(i);
             Coordinate endPoint = pointsList.get(i+1);
             SpatioLine spatioLine = new SpatioLine(startPoint,endPoint,width);
-//            System.out.println(spatioLine.description());
+            decomposeLines.add(spatioLine);
         }
+        return decomposeLines;
     }
-
-//    public static void main(String []args) throws InvalidConstraintException {
-//        List<Coordinate>pointsList = new ArrayList<>();
-//        pointsList.add(new Coordinate(0,1));
-//        pointsList.add(new Coordinate(1,3));
-//        pointsList.add(new Coordinate(2,4));
-//        pointsList.add(new Coordinate(4,2));
-//        pointsList.add(new Coordinate(2,0));
-//        DecomposeSpatioLine decomposeSpatioLine = new DecomposeSpatioLine(2,pointsList);
-//    }
 }
